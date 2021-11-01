47c7010b29a6a14ca81cd599bba61ae81ed01270
Ian F <ian.fiddeman@gmail.com>
2021-11-01 08:10:34 +0000

    unusual_five added

---
+++ Fun/return_5_without_numbers.py
@@ -0,0 +1,19 @@
+
+def unusual_five_og():
+    return len("five!")
+
+#alter solution
+unusual_five = lambda: ord("")
+
+#https://en.wikipedia.org/wiki/ASCII
+
+print(unusual_five() == 5)
+
+
+# Description:
+
+# Write a function that always returns 5
+
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+# Good luck :)
